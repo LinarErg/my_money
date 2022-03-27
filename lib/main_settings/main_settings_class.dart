192b2bca589b451019f709dd9909662c132fa416
ergashevli <link.linar@gmail.com>
2022-03-27 11:42:26 +0300

    add: Main settings class
    Добавлен класс основных настроек

---
+++ lib/main_settings/main_settings_class.dart
@@ -0,0 +1,10 @@
+class MainSettings {
+  final String dataAcquisitionPath;
+  final String loginAuthorization;
+  final String passwordAuthorization;
+
+  MainSettings(
+      {required this.dataAcquisitionPath,
+      required this.loginAuthorization,
+      required this.passwordAuthorization});
+}
