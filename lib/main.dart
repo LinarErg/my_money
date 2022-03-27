0466334c5522969924f984de1e302a0901f12006
ergashevli <link.linar@gmail.com>
2022-03-27 11:01:58 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Мои деньги'),
+          ),
+          body: const Text('временный текст')//const MainScreenWidget(),
+        ),
+      ),
+    );
+  }
+}
