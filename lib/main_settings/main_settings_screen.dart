523a6585d14744fb28a1deb6b756c21218130b1c
ergashevli <link.linar@gmail.com>
2022-03-28 22:54:41 +0300

    add: Authorization setting screen
    Добавлены поля авторизации и пути подключения к http сервису

--- lib/main_settings/main_settings_screen.dart
+++ lib/main_settings/main_settings_screen.dart
@@ -17,9 +17,89 @@ class _MainSettingWidgetState extends State<MainSettingWidget> {
         appBar: AppBar(
           title: const Text('Настройки'),
         ),
-        body: Container(),
-        bottomNavigationBar: BottomNavigationBarWidget(currentIndex: 1,),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            children: const [
+              AuthorizationSettingWidget(),
+              SaveSettingButtonWidget(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBarWidget(
+          currentIndex: 1,
+        ),
       ),
+    );
+  }
+}
+
+class SaveSettingButtonWidget extends StatelessWidget {
+  const SaveSettingButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
+
+class AuthorizationSettingWidget extends StatefulWidget {
+  const AuthorizationSettingWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AuthorizationSettingWidget> createState() =>
+      _AuthorizationSettingWidgetState();
+}
+
+class _AuthorizationSettingWidgetState
+    extends State<AuthorizationSettingWidget> {
+  TextEditingController textEditControllerLogin = TextEditingController();
+  TextEditingController textEditControllerPassword = TextEditingController();
+  TextEditingController textEditControllerPath = TextEditingController();
+  final textFieldDecoration = const InputDecoration(
+    border: OutlineInputBorder(),
+    contentPadding: EdgeInsets.symmetric(horizontal: 5),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          'Login',
+        ),
+        TextField(
+          style: const TextStyle(fontWeight: FontWeight.w600),
+          controller: textEditControllerLogin,
+          decoration: textFieldDecoration,
+          textAlign: TextAlign.center,
+          readOnly: false,
+        ),
+        const Text(
+          'Password',
+        ),
+        TextField(
+          style: const TextStyle(fontWeight: FontWeight.w600),
+          controller: textEditControllerPassword,
+          decoration: textFieldDecoration,
+          textAlign: TextAlign.center,
+          readOnly: false,
+        ),
+        const Text(
+          'Path ',
+        ),
+        TextField(
+          style: const TextStyle(fontWeight: FontWeight.w600),
+          controller: textEditControllerPath,
+          decoration: textFieldDecoration,
+          textAlign: TextAlign.center,
+          readOnly: false,
+        ),
+      ],
     );
   }
 }
